5e7306f451884dc4e2adfebcd83cf9c698563dde
bauscha <Bausch@gmx.net>
2023-01-12 20:51:31 +0100

    Fixes in Header and responsive design

---
+++ docs/script.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function() {
+    getData();
+})
+
+function getData() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        const xmlDoc = xhttp.response.XML;
+        const service = xmlDoc.getElementByTagName("Services");
+        pullData(service);
+    }
+    xhttp.open("GET", "4c-xml-data-file.xml", true);
+    xhttp.send();
+}
+
+
+function pullData(service) {
+    let table="<tr><th>Service</th><th>Pricing</th></tr>";
+    for(let i = 0; i < service.length; i++) {
+        table += "<tr><td>" +
+            service[i].getElementsByTagName("service_name")
+            [0].childNodes[0].nodeValue +
+            "</td><td>" +
+            service[i].getElementsByTagName("package_price")
+            [0].childNodes[0].nodeValue +
+            "</td></tr>";
+    }
+
+    document.getElementById("demo").innerHTML = table;
+}
